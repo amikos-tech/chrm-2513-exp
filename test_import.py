de5cd29e30136b60b08d52a0f2faf563a108da41
Trayan Azarov <trayan.azarov@amikos.tech>
2024-07-13 15:17:56 +0300

    feat: Adding numpy random vectors as embeddings

--- test_import.py
+++ test_import.py
@@ -1,13 +1,13 @@
 import chromadb
-
+import numpy as np
 import uuid
 
 
 def reproduce() -> None:
+    random_vectors = np.random.rand(100, 1024)
     client = chromadb.PersistentClient()
     collection = client.get_or_create_collection("my_collection")
-
-    collection.add(ids=[f"{uuid.uuid4()}" for _ in range(1000)], documents=[f"document {i}" for i in range(1000)])
+    collection.add(ids=[f"{uuid.uuid4()}" for _ in range(1000)], documents=[f"document {i}" for i in range(1000)], embeddings=random_vectors.tolist())
     print("Added 1000 documents to collection")
 
 if __name__ == "__main__":
