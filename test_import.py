3e21e65783ff8a5c5dc31a9a8ff4ae3d5e484d39
Trayan Azarov <trayan.azarov@amikos.tech>
2024-07-13 15:24:17 +0300

    fix: Aligning entries and embeddings

--- test_import.py
+++ test_import.py
@@ -4,11 +4,14 @@ import uuid
 
 
 def reproduce() -> None:
-    random_vectors = np.random.rand(100, 1024)
+    entries = 1000
+    random_vectors = np.random.rand(entries, 1024)
     client = chromadb.PersistentClient()
     collection = client.get_or_create_collection("my_collection")
-    collection.add(ids=[f"{uuid.uuid4()}" for _ in range(1000)], documents=[f"document {i}" for i in range(1000)], embeddings=random_vectors.tolist())
+    collection.add(ids=[f"{uuid.uuid4()}" for _ in range(entries)], documents=[f"document {i}" for i in range(entries)],
+                   embeddings=random_vectors.tolist())
     print("Added 1000 documents to collection")
+
 
 if __name__ == "__main__":
     reproduce()
