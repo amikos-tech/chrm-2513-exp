fdf81dff2b330a546f7910a430607a3e4ed44094
Trayan Azarov <trayan.azarov@amikos.tech>
2024-07-13 14:28:08 +0300

    chore: Add debug message

--- test_import.py
+++ test_import.py
@@ -8,7 +8,7 @@ def reproduce() -> None:
     collection = client.get_or_create_collection("my_collection")
 
     collection.add(ids=[f"{uuid.uuid4()}" for _ in range(1000)], documents=[f"document {i}" for i in range(1000)])
-
+    print("Added 1000 documents to collection")
 
 if __name__ == "__main__":
     reproduce()
