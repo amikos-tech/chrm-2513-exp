57ac40e1cfe33fd58c8fd3ef52b1b76a240c497d
Trayan Azarov <trayan.azarov@amikos.tech>
2024-07-13 14:11:05 +0300

    chore: Trying with conda

--- test_import.py
+++ test_import.py
@@ -7,7 +7,7 @@ def reproduce() -> None:
     client = chromadb.PersistentClient()
     collection = client.get_or_create_collection("my_collection")
 
-    collection.add(ids=[f"{uuid.uuid4()}" for _ in range(100)], documents=[f"document {i}" for i in range(100)])
+    collection.add(ids=[f"{uuid.uuid4()}" for _ in range(1000)], documents=[f"document {i}" for i in range(1000)])
 
 
 if __name__ == "__main__":
